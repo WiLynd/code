1b26cbefc8aedc420ab056cb3f975b1b181e4d94
WiLynd <hdqphu.dev@gmail.com>
2021-11-03 08:52:21 +0700

    [Thực hành] Sử dụng toán tử

---
+++ Biến, kiểu dữ liệu và toán tử/Thực hành/index4.js
@@ -0,0 +1,8 @@
+var inputWidth;
+var inputHeight;
+inputWidth = prompt("Nhập chiều dài:");
+inputHeight = prompt("Nhập chiều cao:");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+alert("Diện tích: " + area);
