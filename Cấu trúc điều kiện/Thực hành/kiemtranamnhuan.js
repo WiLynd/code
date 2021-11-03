739f50027201fce0c4547140203683022df01c2f
WiLynd <hdqphu.dev@gmail.com>
2021-11-03 16:35:21 +0700

    [Thực hành] Kiểm tra năm nhuận

--- Cấu trúc điều kiện/Thực hành/kiemtranamnhuan.js
+++ Cấu trúc điều kiện/Thực hành/kiemtranamnhuan.js
@@ -1,12 +1,11 @@
-let year = parseInt(prompt("Nhập vào một năm bất kỳ:"));
-
-let namnhuan = false;
-
-let Div4 = year % 4 == 0;
-if (Div4) {
-    let Div100 = year % 100 == 0;
+var year = parseInt(prompt("Nhập vào một năm bất kỳ:"));
+var namnhuan = false;
+var Div4 = year % 4 == 0;
+if (Div4)
+{
+    var Div100 = year % 100 == 0;
     if (Div100) {
-        let Div400 = year % 400 == 0;
+        var Div400 = year % 400 == 0;
         if (Div400) {
             namnhuan = true;
         }
@@ -14,8 +13,8 @@ if (Div4) {
         namnhuan = true;
     }
 }
-
-if (namnhuan) {
+if (namnhuan)
+{
     alert(year + " là năm nhuận!");
 } else {
     alert(year + " không phải là năm nhuận!");
